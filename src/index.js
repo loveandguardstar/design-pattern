9f430869d0a59ee345a3aa538ad7418c34f6185d
loveandguardstar <hongjunliao@yeah.net>
2022-03-02 17:44:50 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-alert(200)
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+
+  getName() {
+    alert(`My name is ${this.name}, age is ${this.age}`)
+  }
+
+  eat() {
+    alert(`${this.name} eat something`)
+  }
+}
+
+class Son extends Person {
+  constructor(name, age, number) {
+    super(name, age)
+    this.number = number
+  }
+
+  getCode() {
+    alert(`My name is ${this.name}, My code is ${this.number}`)
+  }
+}
+
+let wang = new Son('wang', 16, 12)
+wang.getCode()
